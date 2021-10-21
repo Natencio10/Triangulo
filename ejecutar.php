02f9e028c60bd8155f0c3f7e294ea9ad10e6d05b
Natencio10 <84600578+Natencio10@users.noreply.github.com>
2021-10-20 23:47:31 -0400

    Add files via upload

---
+++ ejecutar.php
@@ -0,0 +1,10 @@
+<?php
+$catetoA = 3;
+$CatetoB = 4;
+$Hipotenusa = null;
+$Hipotenusa = sqrt(pow($catetoA , 2) + pow($CatetoB, 2));
+echo 'Calculo del área del triángulo', '<br />';
+echo 'Cateto A del Triángulo: ' . $catetoA, '<br />';
+echo 'Cateto B del Triángulo: ' . $CatetoB, '<br />';
+echo 'Hipotenusa del Triángulo: ' . $Hipotenusa, '<br />';
+?>
